d98d82c8f0d72286cbdf0ee6c235616e0858a365
consolekakao <console@kakao.com>
2020-05-19 18:03:42 +0900

    react 1day

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+function App() {
+  
+  return (
+    <div className="App-frame">
+
+          <div className="App-top">
+            top
+          </div>
+
+<div className="App-side">
+  side
+</div>
+
+<div className="App-main">
+  main
+</div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/TOC.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import './App.css';
+class TOC extends Component{
+    render(){
+        return(
+                <div className="App-top">
+                    
+                </div>
+
+        );
+    }
+}
+export default TOC;
